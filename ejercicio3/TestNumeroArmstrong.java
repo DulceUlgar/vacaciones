3cd7ec2fa73d374e4e2c50cc6f395377809bd7af
DulceUlgar <133334486+DulceUlgar@users.noreply.github.com>
2023-08-06 13:54:34 +0200

    ejrcicio 3 con sus test

--- ejercicio3/TestNumeroArmstrong.java
+++ ejercicio3/TestNumeroArmstrong.java
@@ -1,6 +1,11 @@
 package ejercicio3;
+
 public class TestNumeroArmstrong {
     public static void main(String[] args) {
+        // Encabezado de la tabla
+        System.out.println("Número | Es Armstrong");
+        System.out.println("-------------------");
+
         // Ejecutamos algunos tests para probar el programa
         testArmstrong(371); // Es un número de Armstrong
         testArmstrong(153); // Es un número de Armstrong
@@ -10,9 +15,37 @@ public class TestNumeroArmstrong {
     }
 
     public static void testArmstrong(int numero) {
-        System.out.println("Test para el número " + numero + ":");
-        boolean esArmstrong = NumeroArmstrong.esNumeroArmstrong(numero);
-        System.out.println(numero + " es un número de Armstrong: " + esArmstrong);
-        System.out.println("-------------------------");
+        boolean esArmstrong = esNumeroArmstrong(numero);
+        System.out.print(numero);
+        for (int i = 0; i < 7 - contarCifras(numero); i++) {
+            System.out.print(" ");
+        }
+        System.out.print("| ");
+        System.out.println(esArmstrong);
+    }
+
+    public static boolean esNumeroArmstrong(int numero) {
+        int numOriginal = numero;
+        int sumaDigitosElevados = 0;
+        int numCifras = contarCifras(numero);
+
+        while (numero > 0) {
+            int digito = numero % 10;
+            sumaDigitosElevados += Math.pow(digito, numCifras);
+            numero /= 10;
+        }
+
+        return sumaDigitosElevados == numOriginal;
+    }
+
+    public static int contarCifras(int numero) {
+        int cifras = 0;
+
+        while (numero > 0) {
+            cifras++;
+            numero /= 10;
+        }
+
+        return cifras;
     }
 }
